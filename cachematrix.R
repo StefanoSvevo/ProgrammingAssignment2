3569907cb659aeacb13ef35fc8cfda08cabbfa39
Stefano Svevo <stefano.svevo@gmail.com>
2014-06-23 01:08:23 +0200

    Firstdraft

--- cachematrix.R
+++ cachematrix.R
@@ -2,9 +2,29 @@
 ## functions do
 
 ## Write a short comment describing this function
+## makeCacheMatrix creates an object of type cacheMatrix and defines
+## methods to set its internal matrix (set), get its internal matrix (get),
+## cache the Inverse matrix (setInverse) and retrieve the internal matrix (getInverse)
 
 makeCacheMatrix <- function(x = matrix()) {
 
+  I <- NULL
+  
+  set <- function(y) {
+    x <<- y
+    I <<- NULL
+  }
+  
+  get <- function() x
+  
+  setInverse <- function(inverse) I <<- inverse
+  
+  getInverse <- function() I
+  
+  list (set = set, get = get, setInverse = setInverse, getInverse = getInverse)
+  
+}
+  
 }
 
 
